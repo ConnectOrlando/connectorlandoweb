45777afd34dc249e6171a8f69c0541cb8fa5f884
Angie Lam <angielamm13@gmail.com>
2023-04-26 18:24:15 -0400

    saving learning material

---
+++ components/card/index.js
@@ -0,0 +1,31 @@
+import CardStyles from '@app/components/card/Card.module.css';
+
+export default function Card({
+  image,
+  header,
+  subtitle,
+  location,
+  savedConnections,
+  savedBusiness,
+}) {
+  return (
+    <div className="card-container">
+      <img className={CardStyles.roundedImage} src={image} />
+      <h1 className={CardStyles.header}>{header}</h1>
+      <p className={CardStyles.subtitle}>{subtitle}</p>
+      <p className={CardStyles.location}>{location}</p>
+      <button className={CardStyles.button}>Click me</button>
+      <div className={CardStyles.userSaves}>
+        <div className={CardStyles.stat}>
+          <p className={CardStyles.number}>{savedConnections}</p>
+          <p className={CardStyles.label}>{'Saved Connections'}</p>
+        </div>
+        <div className={CardStyles.stat}>
+          <p className={CardStyles.number}>{savedBusiness}</p>
+          <p className={CardStyles.label}>{'Saved Businesses'}</p>
+        </div>
+      </div>
+      <button className={CardStyles.button}>Click me</button>
+    </div>
+  );
+}
