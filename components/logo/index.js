38b29818c8fe79284e315a06fcaaccec08c59fdd
angielamm <97543469+angielamm@users.noreply.github.com>
2023-06-10 20:08:43 -0600

    Created logo component (#6)
    * Created logo component
    
    * Logo sizes, image saved locally
    
    * removed error message

---
+++ components/logo/index.js
@@ -0,0 +1,34 @@
+import LogoStyles from '@app/components/logo/logo.module.css';
+import Image from 'next/image';
+
+export default function Logo({ size }) {
+  let width;
+
+  switch (size) {
+    case 'small': {
+      width = 100;
+      break;
+    }
+    case 'medium': {
+      width = 200;
+      break;
+    }
+    case 'large': {
+      width = 300;
+      break;
+    }
+    default: {
+      width = 200;
+    }
+  }
+
+  return (
+    <Image
+      className={LogoStyles.logo}
+      src={'/images/samsunglogo.png'}
+      width={width}
+      height={0} /*set to auto in css file*/
+      alt={'Connect Orlando Logo'}
+    />
+  );
+}
