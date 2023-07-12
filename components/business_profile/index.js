e0911d511adcfbead4a0570dc274a7249a596829
angielamm <angielamm13@gmail.com>
2023-07-11 22:31:53 -0600

    adjusted sizes and added parameters

--- components/business_profile/index.js
+++ components/business_profile/index.js
@@ -1,18 +1,27 @@
 import BusinessProfileStyles from '@app/components/business_profile/business_profile.module.css';
-import Image from 'next/image';
+import { Label } from 'semantic-ui-react';
 
-export default function BusinessProfileCard() {
+export default function BusinessProfileCard({ url, businessName, category }) {
+  if (url == null) {
+    throw new Error('Component requires url');
+  }
+  if (businessName == null) {
+    throw new Error('Component requires business name');
+  }
+  if (category == null) {
+    throw new Error('Component requires business category');
+  }
   return (
     <div className={BusinessProfileStyles.container}>
-      <Image
-        className={BusinessProfileStyles.businessImage}
-        src={'https://picsum.photos/300'}
-        width={180}
-        height={180}
-        alt={'Business image'}
-      />
-      <h1 className={BusinessProfileStyles.businessName}>Business Name</h1>
-      <button className={BusinessProfileStyles.businessType}>Restaurant</button>
+      <img className={BusinessProfileStyles.businessImage} src={url} />
+      <h1 className={BusinessProfileStyles.businessName}>{businessName}</h1>
+      <Label
+        color="blue"
+        horizontal
+        className={BusinessProfileStyles.businessType}
+      >
+        {category}
+      </Label>
     </div>
   );
 }
