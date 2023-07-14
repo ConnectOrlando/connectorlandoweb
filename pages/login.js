85ad01583ab054881bce3542744a9ec1215defb4
Jesse Najera <jessenajera06@gmail.com>
2023-07-14 18:59:46 -0400

    rewriting login page

--- pages/login.js
+++ pages/login.js
@@ -1,66 +1,92 @@
 import Head from 'next/head';
-import { Button, Checkbox, Form, Header, Segment } from 'semantic-ui-react';
-import LoginStyles from '@app/styles/login.module.css';
-import classNames from 'classnames';
+import loginStyles from '@app/styles/login.module.css';
+import {
+  Header,
+  Segment,
+  Form,
+  Button,
+  Checkbox,
+  Breadcrumb,
+} from 'semantic-ui-react';
+import cx from 'classnames';
 
-export default function Login() {
+export default function login() {
   return (
     <>
       <Head>
         <title>Login - ConnectOrlando</title>
         <meta
-          name="description"
-          content="Login into your ConnectOrlando account"
+          name="desecription"
+          content="Login to your ConnectOrlando account"
         />
       </Head>
-      <main>
-        <Segment className={LoginStyles.segment} raised>
-          <Header as="h1" className={LoginStyles.continueButton}>
-            Sign In
+      <main className={loginStyles.center}>
+        <Segment className={loginStyles.segment}>
+          <Header className={loginStyles.loginHeader} as="h1">
+            Log In
           </Header>
           <Form>
             <Form.Field>
-              <input placeholder="Email or phone number" />
-            </Form.Field>
-            <Form.Field>
-              <input placeholder="Password" />
-            </Form.Field>
-            <Button
-              className={LoginStyles.continueButton}
-              type="submit"
-              color="blue"
-            >
-              Continue
-            </Button>
-            <Form.Field>
-              <Checkbox
-                className={LoginStyles.buttonSpacing}
-                label="Remember me"
+              <label className={loginStyles.infoHeader}>Email</label>
+              <input
+                className={loginStyles.placeholderText}
+                placeholder="name@example.com"
               />
-              <a
-                className={classNames(
-                  LoginStyles.sideSpacing,
-                  LoginStyles.makeBlack
-                )}
-                href="https://www.w3schools.com" //change link to "forgot password" page
-              >
-                Forgot password?
-              </a>
             </Form.Field>
-            <Form.Field className={LoginStyles.center}>
-              New to ConnectOrlando?
-              <a
-                className={classNames(
-                  LoginStyles.singleSpacing,
-                  LoginStyles.newUserButton
-                )}
-                href="https://www.w3schools.com" //change link to "new user" page
-              >
-                Sign up now.
-              </a>
+            <Form.Field>
+              <label className={loginStyles.infoHeader}>Password</label>
+              <input placeholder="****************"></input>
             </Form.Field>
           </Form>
-          <Form.Field></Form.Field>
+          <Form.Field className={loginStyles.verticalSpacing}>
+            <Button
+              type="submit"
+              fluid
+              color="blue"
+              className={cx(loginStyles.loginButton, loginStyles.capitalize)}
+            >
+              log in
+            </Button>
+          </Form.Field>
+          <Form.Field className={loginStyles.verticalSpacing}>
+            <Checkbox
+              className={loginStyles.rememberMeButton}
+              label="Remember me"
+            ></Checkbox>
+            <Breadcrumb.Section
+              className={cx(
+                loginStyles.forgotPasswordBreadcrumb,
+                loginStyles.toRight
+              )}
+              a
+              href="https://cdn.wallpapersafari.com/74/46/dr3t5f.jpg"
+              link
+            >
+              Forgot Password?
+            </Breadcrumb.Section>
+          </Form.Field>
+          <Form.Field className={loginStyles.verticalSpacing}>
+            <Header
+              className={cx(
+                loginStyles.center,
+                loginStyles.decreaseBottomMargin
+              )}
+              as="h4"
+            >
+              Don&apos;t have an account?
+            </Header>
+            <Breadcrumb.Section
+              className={cx(
+                loginStyles.center,
+                loginStyles.signUpNowBreadcrumb
+              )}
+              a
+              href="https://cdn.wallpapersafari.com/26/94/LNjd52.jpg"
+              link
+            >
+              Sign up now.
+            </Breadcrumb.Section>
+          </Form.Field>
         </Segment>
       </main>
     </>
