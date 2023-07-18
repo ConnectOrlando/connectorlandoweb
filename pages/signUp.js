92f699db0a89095fc63560894e5b69506fc3cf65
Jesse Najera <jessenajera06@gmail.com>
2023-07-18 14:42:03 -0400

    made sign up page

---
+++ pages/signUp.js
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+import signupStyles from '@app/styles/signUp.module.css';
+import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import cx from 'classnames';
+import LabeledInput from '@app/components/pieces/labeledInput';
+
+export default function signUp() {
+  return (
+    <>
+      <Head>
+        <title>Sign up - ConnectOrlando</title>
+        <meta name="description" content="Create an account" />
+      </Head>
+      <main className={signupStyles.center}>
+        <Segment basic className={signupStyles.segment}>
+          <Header
+            className={cx(signupStyles.pageTitle, signupStyles.center)}
+            as="h1"
+          >
+            Create Your Account
+          </Header>
+          <p className={signupStyles.pageDescription}>
+            You will use this email and password to log into your ConnectOrlando
+            account.
+          </p>
+
+          <LabeledInput label="Email" />
+
+          <Form.Field className={signupStyles.verticalSpacing}>
+            <LabeledInput label="Password" />
+          </Form.Field>
+
+          <Form.Field className={signupStyles.verticalSpacing}>
+            <LabeledInput label="Name" />
+          </Form.Field>
+
+          <Button
+            color="blue"
+            fluid
+            className={cx(signupStyles.continueButton, signupStyles.capitalize)}
+          >
+            Continue
+          </Button>
+        </Segment>
+      </main>
+    </>
+  );
+}
