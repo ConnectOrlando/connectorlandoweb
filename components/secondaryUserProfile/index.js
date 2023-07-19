ce61ae699fb189934e19baa818e17336e9c724ba
angielamm <angielamm13@gmail.com>
2023-07-18 23:52:02 -0600

    created secondary user profile card

---
+++ components/secondaryUserProfile/index.js
@@ -0,0 +1,54 @@
+import UserProfileStyles from '@app/components/secondaryUserProfile/secondaryUserProfile.module.css';
+import { Segment, Header, Image } from 'semantic-ui-react';
+import _ from 'lodash-es';
+
+export default function SecondaryUserProfile({ imageUrl, name, location }) {
+  if (!_.isString(imageUrl || _.isEmpty(imageUrl))) {
+    throw new TypeError('Component requires a profile picture');
+  }
+  if (!_.isString(name) || _.isEmpty(name)) {
+    throw new Error('Component requires user name');
+  }
+  if (!_.isString(location) || _.isEmpty(location)) {
+    throw new Error('Component requires a location');
+  }
+  let titleFontSize = 12;
+  let subtitleFontSize = 8;
+  const words = name.split(' ');
+  for (const word of words) {
+    if (word.length > 7) {
+      const extraChars = name.length - 7;
+      const sizeDecrease = extraChars * 2;
+      titleFontSize -= sizeDecrease;
+    }
+    if (titleFontSize < 9) {
+      titleFontSize = 9;
+      subtitleFontSize = 7;
+      break;
+    }
+  }
+  return (
+    <Segment className={UserProfileStyles.container}>
+      <Image
+        className={UserProfileStyles.userImage}
+        alt="User profile picture"
+        circular
+        src={imageUrl}
+      />
+      <div className={UserProfileStyles.right}>
+        <Header
+          className={UserProfileStyles.userName}
+          style={{ fontSize: `${titleFontSize}px` }}
+        >
+          {name}
+        </Header>
+        <p
+          className={UserProfileStyles.location}
+          style={{ fontSize: `${subtitleFontSize}px` }}
+        >
+          {location}{' '}
+        </p>
+      </div>
+    </Segment>
+  );
+}
