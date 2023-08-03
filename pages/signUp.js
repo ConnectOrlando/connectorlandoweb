a1f90fd7dab755d29e17e8f38de6914ac26ab92d
Jesse Najera <jessenajera06@gmail.com>
2023-08-03 12:38:04 -0400

    styling changes

--- pages/signUp.js
+++ pages/signUp.js
@@ -13,12 +13,14 @@ export default function signUp() {
       </Head>
       <main className={signupStyles.center}>
         <Segment basic className={signupStyles.segment}>
-          <Header
-            className={cx(signupStyles.pageTitle, signupStyles.center)}
-            as="h1"
-          >
-            Create Your Account
-          </Header>
+          <Form.Field>
+            <Header
+              className={cx(signupStyles.pageTitle, signupStyles.center)}
+              as="h1"
+            >
+              Create Your Account
+            </Header>
+          </Form.Field>
           <p className={signupStyles.pageDescription}>
             You will use this email and password to log into your ConnectOrlando
             account.
@@ -50,6 +52,17 @@ export default function signUp() {
             >
               Create Account
             </Button>
+            <Form.Field className={signupStyles.center}>
+              <Header className={signupStyles.loginHeader} as="h4">
+                Already have an account?
+              </Header>
+              <a
+                className={signupStyles.loginAnchor}
+                href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FO6gkm-nklyw%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=72841007a96142d220bce3d50ec909c3e775a987ac2fd906048991a56ad59afa&ipo=images"
+              >
+                Log in
+              </a>
+            </Form.Field>
           </Form>
         </Segment>
       </main>
