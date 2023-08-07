cd75e43064a254d1924e145e826fe72061c88e66
angielamm <angielamm13@gmail.com>
2023-08-07 13:44:14 -0600

    udpated parameter, proptypes, adjusted height

--- components/userDescriptionCard/index.js
+++ components/userDescriptionCard/index.js
@@ -1,28 +1,32 @@
 import CardStyles from '@app/components/userDescriptionCard/userDescriptionCard.module.css';
 import { Segment, Button } from 'semantic-ui-react';
-import UserCard from '../userCard';
+import UserCard from '@app/components/UserCard';
+import PropTypes from 'prop-types';
 
-export function UserDescriptionCard({ userDescription }) {
+UserDescriptionCard.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    profileImage: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      street: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      state: PropTypes.string.isRequired,
+      zip: PropTypes.string.isRequired,
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export function UserDescriptionCard({ user }) {
+  const { name, profileImage, location } = user;
+  const cardInfo = { name, profileImage, location };
+
+  const { description } = user;
   return (
     <Segment padded className={CardStyles.container}>
-      <UserCard
-        profileImage=""
-        name="Camila Gonzalez"
-        location={{
-          street: '123 Main St',
-          city: 'Winter Park',
-          state: 'FL',
-          zip: '32789',
-        }}
-      ></UserCard>
-      <p className={CardStyles.userDescription}>{userDescription}</p>
-      <Button
-        compact
-        color="blue"
-        className={
-          CardStyles.button
-        } /*will use propStyles to define as string*/
-      >
+      <UserCard {...cardInfo}></UserCard>
+      <p className={CardStyles.userDescription}> {description} </p>
+      <Button compact color="blue" className={CardStyles.button}>
         More Info
       </Button>
     </Segment>
