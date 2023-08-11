818b32aea8345e797d6a2a19ac034caa7f596a82
jnajera456 <138731413+jnajera456@users.noreply.github.com>
2023-08-11 13:55:12 -0400

    Sign-in page (#12)
    * starting work on the login page
    
    * rewriting login page
    
    * added login page
    
    * LabeledInput component added to login page, styling changes
    
    * made login page use labeledInput component, styling changes
    
    * styling changes
    
    * styling changes, put elementin a form and labeledInputs in individual form fields
    
    * styling changes
    
    * styling changes, anchor tags
    
    * styling changes
    
    * styling changes
    
    * styling changes
    
    ---------
    
    Co-authored-by: Jesse Najera <jessenajera@Pablos-iMac.local>
    Co-authored-by: Jesse Najera <jessenajera06@gmail.com>

---
+++ pages/login.js
@@ -0,0 +1,77 @@
+import Head from 'next/head';
+import loginStyles from '@app/styles/login.module.css';
+import { Header, Segment, Form, Button, Checkbox } from 'semantic-ui-react';
+import cx from 'classnames';
+import LabeledInput from '@app/components/pieces/labeledInput';
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Sign In - ConnectOrlando</title>
+        <meta
+          name="description"
+          content="Sign in to your ConnectOrlando account"
+        />
+      </Head>
+      <main className={loginStyles.center}>
+        <Segment basic className={loginStyles.segment}>
+          <Header
+            className={cx(loginStyles.loginHeader, loginStyles.center)}
+            as="h1"
+          >
+            Log In
+          </Header>
+          <Form>
+            <Form.Field>
+              <LabeledInput label="Email" placeholder="name@example.com" />
+            </Form.Field>
+
+            <Form.Field>
+              <LabeledInput label="Password" placeholder="****************" />
+            </Form.Field>
+
+            <Form.Field className={loginStyles.topSpacing}>
+              <Button
+                type="submit"
+                fluid
+                color="blue"
+                className={loginStyles.loginButton}
+              >
+                log in
+              </Button>
+            </Form.Field>
+            <Form.Field
+              className={cx(loginStyles.topSpacing, loginStyles.spaceBetween)}
+            >
+              <Checkbox
+                className={loginStyles.rememberMeButton}
+                label="Remember me"
+              ></Checkbox>
+              <a
+                className={loginStyles.forgotPasswordAnchor}
+                href="https://cdn.wallpapersafari.com/74/46/dr3t5f.jpg"
+              >
+                Forgot Password?
+              </a>
+            </Form.Field>
+            <Form.Field className={loginStyles.topSpacing}>
+              <Header
+                className={cx(loginStyles.questionHeader, loginStyles.center)}
+                as="h5"
+              >
+                Don&apos;t have an account?
+              </Header>
+              <a
+                className={cx(loginStyles.center, loginStyles.signUpNowAnchor)}
+                href="https://cdn.wallpapersafari.com/26/94/LNjd52.jpg"
+              >
+                Sign up now
+              </a>
+            </Form.Field>
+          </Form>
+        </Segment>
+      </main>
+    </>
+  );
+}
