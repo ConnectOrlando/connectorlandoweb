2dee23ac450318163d8a92d08a5d06c92bdfc924
angielamm <97543469+angielamm@users.noreply.github.com>
2023-08-15 09:57:40 -0600

    Updated Description Component (#22)
    updated component

--- components/description/index.js
+++ components/description/index.js
@@ -12,27 +12,12 @@ export default function Description({ title, text }) {
   if (_.isEmpty(title)) {
     throw new Error('Component requires title');
   }
-
-  let titleFontSize = 25;
-  const words = title.split(' ');
-  for (const word of words) {
-    if (word.length > 5) {
-      const extraChars = title.length - 5;
-      const sizeDecrease = extraChars * 2;
-      titleFontSize -= sizeDecrease;
-    }
-  }
   return (
     <Segment className={DescriptionStyles.container}>
-      <div className={DescriptionStyles.left}>
-        <h1
-          className={DescriptionStyles.title}
-          style={{ fontSize: `${titleFontSize}px` }}
-        >
-          {title}
-        </h1>
+      <div className={DescriptionStyles.header}>
+        <h1 className={DescriptionStyles.title}>{title}</h1>
       </div>
-      <div className={DescriptionStyles.right}>
+      <div className={DescriptionStyles.bottom}>
         <Segment basic className={DescriptionStyles.text}>
           {text}
         </Segment>
