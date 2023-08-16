7bd13ba7ec687c6f34e3c8d5d9c15279f9b0c59c
Jesse Najera <jessenajera06@gmail.com>
2023-08-16 13:05:04 -0400

    file name changes

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import signupStyles from '@app/styles/signup.module.css';
+import registerStyles from '@app/styles/register.module.css';
 import { Header, Segment, Form, Button } from 'semantic-ui-react';
 import cx from 'classnames';
 import LabeledInput from '@app/components/pieces/labeledInput';
@@ -11,22 +11,22 @@ export default function signup() {
         <title>Sign up - ConnectOrlando</title>
         <meta name="description" content="Create an account" />
       </Head>
-      <main className={signupStyles.center}>
-        <Segment basic className={signupStyles.segment}>
+      <main className={registerStyles.center}>
+        <Segment basic className={registerStyles.segment}>
           <Form.Field>
             <Header
-              className={cx(signupStyles.pageTitle, signupStyles.center)}
+              className={cx(registerStyles.pageTitle, registerStyles.center)}
               as="h1"
             >
               Create Your Account
             </Header>
           </Form.Field>
-          <p className={signupStyles.pageDescription}>
+          <p className={registerStyles.pageDescription}>
             You will use this email and password to log into your ConnectOrlando
             account.
           </p>
           <Form>
-            <Form.Field className={signupStyles.topSpacing}>
+            <Form.Field className={registerStyles.topSpacing}>
               <LabeledInput label="Name" />
             </Form.Field>
 
@@ -34,11 +34,11 @@ export default function signup() {
               <LabeledInput label="Email" />
             </Form.Field>
 
-            <Form.Field className={signupStyles.topSpacing}>
+            <Form.Field className={registerStyles.topSpacing}>
               <LabeledInput label="Password" />
             </Form.Field>
 
-            <Form.Field className={signupStyles.topSpacing}>
+            <Form.Field className={registerStyles.topSpacing}>
               <LabeledInput label="Confirm Password" />
             </Form.Field>
 
@@ -46,20 +46,20 @@ export default function signup() {
               color="blue"
               fluid
               className={cx(
-                signupStyles.continueButton,
-                signupStyles.capitalize
+                registerStyles.continueButton,
+                registerStyles.capitalize
               )}
             >
               Create Account
             </Button>
             <Form.Field
-              className={cx(signupStyles.center, signupStyles.topSpacing)}
+              className={cx(registerStyles.center, registerStyles.topSpacing)}
             >
-              <Header className={signupStyles.loginHeader} as="h4">
+              <Header className={registerStyles.loginHeader} as="h4">
                 Already have an account?
               </Header>
               <a
-                className={signupStyles.loginAnchor}
+                className={registerStyles.loginAnchor}
                 href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FO6gkm-nklyw%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=72841007a96142d220bce3d50ec909c3e775a987ac2fd906048991a56ad59afa&ipo=images"
               >
                 Log in
