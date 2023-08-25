0538b3779d9829496c6f61fddc77636ec8151d80
Kevin Huang <kevinhuang0126@gmail.com>
2023-08-25 14:30:38 -0400

    New email confirmation page and eslint windows error fix

---
+++ pages/confirmEmail.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import confirmEmailStyles from '@app/styles/confirmEmail.module.css';
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default function confirmEmail() {
+  return (
+    <>
+      <Head>
+        <title>Confrim Email - ConnectOrlando</title>
+        <meta name="description" content="Confirmed email page" />
+      </Head>
+      <main className={confirmEmailStyles.center}>
+        <img
+          src="https://slang.net/img/slang/lg/blue-checked_6094.png"
+          alt="checkmark"
+          className={confirmEmailStyles.resizeSmaller}
+        ></img>
+        <h1>
+          <strong>Email Verified!</strong>
+        </h1>
+        <br></br>
+        <p>
+          <em>Your email has now been verified!</em>
+        </p>
+        <p>
+          <em>Enjoy your experience on ConnectOrlando!</em>
+        </p>
+        <div>
+          <Button animated="fade">
+            <Button.Content visible>Click here to return home</Button.Content>
+            <Button.Content hidden>Home</Button.Content>
+          </Button>
+        </div>
+      </main>
+    </>
+  );
+}
