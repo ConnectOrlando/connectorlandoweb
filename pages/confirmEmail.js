95ac6f25ced0a3561f97d5e0e2ad1690c82c0290
Kevin Huang <kevinhuang0126@gmail.com>
2023-08-31 16:25:08 -0400

    comment changes

--- pages/confirmEmail.js
+++ pages/confirmEmail.js
@@ -2,35 +2,30 @@ import Head from 'next/head';
 import confirmEmailStyles from '@app/styles/confirmEmail.module.css';
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
+import Link from 'next/link';
 
 export default function confirmEmail() {
   return (
     <>
       <Head>
         <title>Confirm Email - ConnectOrlando</title>
-        <meta name="description" content="Confirmed email page" />
+        <meta name="description" content="Email Confirmation" />
       </Head>
       <main className={confirmEmailStyles.center}>
-        <img
-          src="https://slang.net/img/slang/lg/blue-checked_6094.png"
-          alt="checkmark"
-          className={confirmEmailStyles.checkmark}
-        ></img>
-        <h1>
-          <strong>Email Verified!</strong>
-        </h1>
+        <Icon name="check circle" size="massive" />
+
+        <h1>Email Verified!</h1>
         <br></br>
-        <p>
-          <em>Your email has now been verified!</em>
-        </p>
-        <p>
-          <em>Enjoy your experience on ConnectOrlando!</em>
-        </p>
+        <p>Your email has now been verified!</p>
+        <p>Enjoy your experience on ConnectOrlando!</p>
         <div>
-          <Button animated="fade">
-            <Button.Content visible>Click here to return home</Button.Content>
-            <Button.Content hidden>Home</Button.Content>
-          </Button>
+          <Link href="/home">
+            <Button animated="fade">
+              <Button.Content visible>Click here to return home</Button.Content>
+              <Button.Content hidden>Done</Button.Content>
+            </Button>
+          </Link>
         </div>
       </main>
     </>
