394edc8f63320fded780250e377a802362ee6bdb
Pablo Najera <pablonajera@users.noreply.github.com>
2023-09-05 22:52:39 -0400

    Integrating API for register route (#25)

--- components/pieces/labeledInput/index.js
+++ components/pieces/labeledInput/index.js
@@ -1,18 +1,53 @@
 import labeledInputStyles from '@app/components/pieces/labeledInput/labeledInput.module.css';
+import { Input, Label } from 'semantic-ui-react';
+import _ from 'lodash-es';
 
-export default function LabeledInput({ label, placeholder, onChange = null }) {
+export default function LabeledInput({
+  label,
+  placeholder,
+  description = '',
+  type = '',
+  errorMessage = '',
+  disabled = false,
+  onChange = null,
+  onBlur = null,
+  icon = '',
+  onIconClick = null,
+}) {
   if (label == null || label === '') {
     throw new Error('Component requires label');
+  }
+  if (!type) {
+    type = 'text';
   }
 
   return (
     <div>
       <label className={labeledInputStyles.label}>{label}</label>
-      <input
-        className={labeledInputStyles.placeholder}
+      {description && (
+        <p className={labeledInputStyles.description}>{description}</p>
+      )}
+      <Input
         placeholder={placeholder}
         onChange={onChange}
+        disabled={disabled}
+        onBlur={onBlur}
+        error={!_.isEmpty(errorMessage)}
+        type={type}
+        action={
+          icon
+            ? {
+                icon,
+                onClick: () => (onIconClick ? onIconClick() : null),
+              }
+            : null
+        }
       />
+      {errorMessage && (
+        <Label basic color="red" size="small" pointing="above">
+          {errorMessage}
+        </Label>
+      )}
     </div>
   );
 }
